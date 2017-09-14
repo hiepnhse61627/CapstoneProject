c31dda9b18be2f463846409e66572a8067e5ef01
DATCTSE61762 <theguncrusher@gmal.com>
2017-09-14 12:12:58 +0700

    Initiate Search student

--- 04.Source/CapstoneMVC/src/main/java/com/capstone/controllers/HomeController.java
+++ 04.Source/CapstoneMVC/src/main/java/com/capstone/controllers/HomeController.java
@@ -1,7 +1,11 @@
 package com.capstone.controllers;
 
+import com.capstone.entities.Student;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -11,6 +15,25 @@ public class HomeController {
         ModelAndView view = new ModelAndView("Index");
         view.addObject("title", "Main");
         view.addObject("message", "Welcome!");
+
+        return view;
+    }
+
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public ModelAndView Search() {
+        ModelAndView view = new ModelAndView("Search");
+        view.addObject("title", "Search");
+
+        return view;
+    }
+
+    @RequestMapping(value = "/search", method = RequestMethod.POST)
+    public ModelAndView SearchByRollNumber(@RequestParam String txtSearch) {
+        ModelAndView view = new ModelAndView("Search");
+        view.addObject("title", "Search");
+        Student stu = new Student();
+        stu.setFirstName(txtSearch);
+        view.addObject("student", stu);
 
         return view;
     }
