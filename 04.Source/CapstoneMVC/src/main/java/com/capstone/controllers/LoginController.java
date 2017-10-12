95ea584b9042d5fc2385ec869d74503c974d2861
DATCTSE61762 <theguncrusher@gmal.com>
2017-10-12 11:24:25 +0700

    Initiate Login

---
+++ 04.Source/CapstoneMVC/src/main/java/com/capstone/controllers/LoginController.java
@@ -0,0 +1,33 @@
+package com.capstone.controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/admin/index")
+    public String Admin() {
+        return "Admin";
+    }
+
+    @RequestMapping("/deny")
+    public String Deny() {
+        return "Deny";
+    }
+
+    @RequestMapping(value="/logout")
+    public String Logout (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/";
+    }
+}
---
+++ 04.Source/CapstoneMVC/web/WEB-INF/Views/Deny.jsp
@@ -0,0 +1,9 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Rem
+  Date: 10/12/2017
+  Time: 11:17 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<h1>DENY</h1>
