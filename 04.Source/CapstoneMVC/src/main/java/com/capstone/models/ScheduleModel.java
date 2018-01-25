d50e887107f62817ca662d20eef1e1259ec77410
LongPham <long.phamlp94@gmail.com>
2018-01-25 21:10:36 +0700

    display many table

---
+++ 04.Source/CapstoneMVC/src/main/java/com/capstone/models/ScheduleModel.java
@@ -0,0 +1,65 @@
+package com.capstone.models;
+
+import com.capstone.entities.DaySlotEntity;
+import com.capstone.entities.EmployeeEntity;
+import com.capstone.entities.RoomEntity;
+
+import java.util.List;
+
+public class ScheduleModel {
+    private String date;
+    private String room;
+    private String courseName;
+    private String slot;
+    private String endTime;
+    private String startTime;
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getSlot() {
+        return slot;
+    }
+
+    public void setSlot(String slot) {
+        this.slot = slot;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String couresName) {
+        this.courseName = couresName;
+    }
+}
+
